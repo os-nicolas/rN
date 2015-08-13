46af2879e1e6a3e3d576bdefda5803429f930512
Colin <ColinWielga@gmail.com>
2015-08-13 01:26:08 -0400

    THIS IS THE COMMIT BEFORE I RECK EVERYTHING

--- app/src/main/java/cube/d/n/rn/FaceIndex.java
+++ app/src/main/java/cube/d/n/rn/FaceIndex.java
@@ -1,5 +1,7 @@
 package cube.d.n.rn;
 
+import android.util.Log;
+
 import java.util.ArrayList;
 
 
@@ -29,26 +31,41 @@ public class FaceIndex extends ArrayList<FaceIndex.FaceValue> {
     //TODO
     //TODO
 
-    public ArrayList<Vector> getCompnetVectors(HasVectorDims tess) {
-        ArrayList<Vector> result = new ArrayList<>();
-
-        for (int i=0;i< size();i++){
-            Vector vec=tess.vectorForDimension(i);
-            vec.scale(get(i),false);
-            if (vec.noneZero()) {
-                result.add(vec);
-            }
-        }
-        return result;
-    }
+//    public ArrayList<Vector> getCompnetVectors(HasVectorDims tess) {
+//        ArrayList<Vector> result = new ArrayList<>();
+//
+//        for (int i=0;i< size();i++){
+//            Vector vec=tess.vectorForDimension(i);
+//            vec.scale(get(i),false);
+//            if (vec.noneZero()) {
+//                result.add(vec);
+//            }
+//        }
+//        return result;
+//    }
 
     @Override
     public int hashCode() {
         int hashCode = 0;
         for (int i = 0; i < size(); i++) {
-            hashCode += (int)Math.pow(RN.rn().getMaxSize(),i) * get(i);
+            hashCode += (int)Math.pow(RN.rn().getMaxSize(),i) * (toInt(get(i)));
         }
         return hashCode;
+    }
+
+    private int toInt(FaceValue fv) {
+        if (fv== FaceValue.BACK){
+            return 0;
+        }else if (fv== FaceValue.FORWARD){
+            return 1;
+        }else if (fv == FaceValue.EVEN){
+            return 2;
+        }else if (fv == FaceValue.NONE){
+            return 3;
+        }
+        Log.e("toInt","unexpected value "+ fv );
+        return -1;
+
     }
 
     @Override
@@ -67,30 +84,30 @@ public class FaceIndex extends ArrayList<FaceIndex.FaceValue> {
     @Override
     public String toString() {
         String result = "(";
-        for (Integer i: this){
+        for (FaceValue i: this){
             result += i + (indexOf(i)!=size()-1?",":"");
         }
         return result+")";
     }
 
-    public int noneZeroComps() {
-        int count = 0;
-        for (int i:this){
-            count += (i!=0?1:0);
-        }
-        return count;
-    }
-
-    public Index rotate(int dim1, int dim2, boolean direction,Tess owner) {
-        Index newIndex = new Index(this);
-        if (direction){
-            newIndex.set(dim1,owner.size.get()-1-this.get(dim2));
-            newIndex.set(dim2,this.get(dim1));
-        }else {
-            newIndex.set(dim1,this.get(dim2));
-            newIndex.set(dim2,owner.size.get()-1-this.get(dim1));
-        }
-        return newIndex;
-    }
+//    public int noneZeroComps() {
+//        int count = 0;
+//        for (FaceValue i:this){
+//            count += (i!= FaceValue.NONE?1:0);
+//        }
+//        return count;
+//    }
+//
+//    public Index rotate(int dim1, int dim2, boolean direction,Tess owner) {
+//        Index newIndex = new Index(this);
+//        if (direction){
+//            newIndex.set(dim1,owner.size.get()-1-this.get(dim2));
+//            newIndex.set(dim2,this.get(dim1));
+//        }else {
+//            newIndex.set(dim1,this.get(dim2));
+//            newIndex.set(dim2,owner.size.get()-1-this.get(dim1));
+//        }
+//        return newIndex;
+//    }
 
 }
